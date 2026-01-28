6783619469a0e61541b1d5a7b183e569ba6698d9
A5TR0spud <57902759+A5TR0spud@users.noreply.github.com>
2026-01-27 22:04:46 -0500

    batch: check other appdata if in "wrong" place

--- build.bat
+++ build.bat
@@ -17,10 +17,13 @@ powershell -Command "Compress-Archive -Path 'builds\*' -DestinationPath 'builds\
 rmdir "builds\Server" /s /q
 rmdir "builds\Common" /s /q
 del "builds\manifest.json" /q
-if "%~dp0.."=="mods\" do (
-    if "%~dp0..\..\.."=="Saves\" do (
-        del "%~dp0..\..\..\..\Mods\A5TR0spud.Flamethrowers*.zip" /q
-        copy "builds\A5TR0spud.Flamethrowers-!v!.zip" %~dp0..\..\..\..\Mods
+if "%~dp0..\..\.."=="mods\*\Saves\" (
+    del "%~dp0..\..\..\..\Mods\A5TR0spud.Flamethrowers*.zip" /q
+    copy "builds\A5TR0spud.Flamethrowers-!v!.zip" %~dp0..\..\..\..\Mods
+) else (
+    if exist "%userprofile%\AppData\Roaming\Hytale\UserData\Mods\" (
+        del "%userprofile%\AppData\Roaming\Hytale\UserData\Mods\A5TR0spud.Flamethrowers*.zip" /q
+        copy "builds\A5TR0spud.Flamethrowers-!v!.zip" %userprofile%\AppData\Roaming\Hytale\UserData\Mods
     )
 )
 endlocal
