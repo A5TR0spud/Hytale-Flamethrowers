2a805d4d38583398cf49de93dca6587444ba6364
A5TR0spud <57902759+A5TR0spud@users.noreply.github.com>
2026-01-29 20:29:09 -0500

    batch: fix old builds being included in new ones

--- build.bat
+++ build.bat
@@ -2,21 +2,20 @@
 SETLOCAL EnableDelayedExpansion
 mkdir "builds"
 powershell -Command "&{Remove-Item builds\* -Exclude '*.zip' -Recurse -Force}
+mkdir "builds\building"
 set v="indev"
-> "builds\ver.tmp" powershell -Nop -C "(Get-Content .\manifest.json|ConvertFrom-Json).Version"&&<"builds\ver.tmp" (set /p v=)&&del "builds\ver.tmp" /q
-> "builds\manifest.json" (
+> "builds\building\ver.tmp" powershell -Nop -C "(Get-Content .\manifest.json|ConvertFrom-Json).Version"&&<"builds\building\ver.tmp" (set /p v=)&&del "builds\building\ver.tmp" /q
+> "builds\building\manifest.json" (
     FOR /f "delims=*" %%a IN (manifest.json) DO (
         set b=%%a
         echo !b:Indev=!
     )
 )
-xcopy "Server" "builds\Server\" /e
-xcopy "Common" "builds\Common\" /e
+xcopy "Server" "builds\building\Server\" /e
+xcopy "Common" "builds\building\Common\" /e
 del "builds\A5TR0spud.Flamethrowers-!v!.zip" /q
-powershell -Command "Compress-Archive -Path 'builds\*' -DestinationPath 'builds\A5TR0spud.Flamethrowers-!v!.zip'"
-rmdir "builds\Server" /s /q
-rmdir "builds\Common" /s /q
-del "builds\manifest.json" /q
+powershell -Command "Compress-Archive -Path 'builds\building\*' -DestinationPath 'builds\A5TR0spud.Flamethrowers-!v!.zip'"
+rmdir "builds\building\" /s /q
 if "%~dp0..\..\.."=="mods\*\Saves\" (
     del "%~dp0..\..\..\..\Mods\A5TR0spud.Flamethrowers*.zip" /q
     copy "builds\A5TR0spud.Flamethrowers-!v!.zip" %~dp0..\..\..\..\Mods
